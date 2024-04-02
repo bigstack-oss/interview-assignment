739d82bd9b07ac6c0a0f0da6832b7c1900bc7410
James C <jamesc09102178@gmail.com>
2024-04-01 23:04:59 -0400

    feat: add placeholder fetch request in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Theme } from '@carbon/react';
 import { BrowserRouter } from 'react-router-dom';
 import UIHeader from '@components/UIHeader';
 import '@src/app.scss';
 
 const App = () => {
+  // TODO: replace this with your own data fetching code!
+  useEffect(() => {
+    function getData() {
+      fetch('http://localhost:9080/instances')
+        .then(res => {
+          if (res.ok) {
+            return res.json();
+          } else {
+            console.error(res.status);
+          }
+        })
+        .then(data => {
+          console.log(data);
+        });
+    }
+
+    getData();
+  }, []);
+
   return (
     <BrowserRouter>
       <Theme theme="g90">
