5c39aa1e87587f1cd882096ae1a70ae9f899e3c3
James C <jamesc09102178@gmail.com>
2024-04-02 11:13:35 -0400

    docs: add JSDOC to utility functions

--- server/routers/utils.js
+++ server/routers/utils.js
@@ -1,5 +1,9 @@
 const PORT = parseInt(process.env.PORT, 10);
 
+/**
+ * a utility function that will return an alternating true/false value every 20 seconds
+ * @returns {boolean} true or false depending on the current server clock time
+ */
 const twentySecondsInterval = () => {
   const now = new Date();
   const minutes = now.getMinutes();
@@ -11,6 +15,12 @@ const twentySecondsInterval = () => {
   return minsFlag ? secondsFlag : !secondsFlag;
 };
 
+/**
+ * utility function to transform instance Data on a fixed time interval,
+ * only switching one property (instance status) of a single instance
+ * @param {object} data - object containing data for multiple instances
+ * @returns {object} - transformed data
+ */
 const transformOnInterval = data => {
   let json = [];
   try {
